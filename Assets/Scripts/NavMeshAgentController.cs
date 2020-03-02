f404506f21b904138111d417640ade67e1bc8a45
Sebastian <tengotreecreepers@gmail.com>
2020-03-01 19:09:30 -0800

    Created a basic navmesh player controller

---
+++ Assets/Scripts/NavMeshAgentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10;
+
+    private NavMeshAgent agent;
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+        //direction.Normalize();
+        agent.velocity = direction;
+    }
+}
